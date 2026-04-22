1a69a8e5c979ae76ad263877baf71647821addb3
grantneumanoracle <grant.neuman@oracle.com>
2026-04-22 11:39:37 -0700

    Release v0.0.7

--- ai-accelerator-tf/vars.tf
+++ ai-accelerator-tf/vars.tf
@@ -418,7 +418,7 @@ variable "oci_tag_values" {
 
 variable "accelerator_pack_stack_version" {
   type        = string
-  default     = "v0.0.6"
+  default     = "v0.0.7"
   description = "Stack release version for AI Accelerator Starter Packs"
 }
 
